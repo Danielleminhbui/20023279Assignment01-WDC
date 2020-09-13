df6527098e46a892a761fbd788047f1c5b67fb90
danielleminhbui <danielleminhbui@gmail.com>
2020-09-13 18:28:07 +1000

    question 3

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@
 // When the server is started, this is the first JavaScript code which will run.
 
 // Use Babel to provide support for JSX
-require('babel-core/register');
+require("babel-core/register");
 
 // Require our Express app (see app.js)
-const app = require('./app');
+const app = require("./app");
 
 // Start the server and wait for connections
-const server = app.listen(3000, () => {
-  console.log('Server started.');
+const server = app.listen(4548, () => {
+  console.log("Server started.");
 });
 
 // Export the server (can be useful for testing purposes)
